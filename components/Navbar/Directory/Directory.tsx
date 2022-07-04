0e1e4f71768a92b751f26b7362906e7a47da869b
jule <71754820+gwish1n@users.noreply.github.com>
2022-07-04 13:51:01 +0200

    Update Directory.tsx

--- components/Navbar/Directory/Directory.tsx
+++ components/Navbar/Directory/Directory.tsx
@@ -12,7 +12,7 @@ const Directory:React.FC = () => {
     return(
     <Menu>
         <MenuButton mr ={2} ml={{base: 0, md: 2}} cursor='pointer' padding ='0px 6px' borderRadius={4} _hover={{outline: '1px solid', outlineColor: 'grey.200'}}>
-        <Flex align="center"j justify ='space-between' widt={{base: "autho", lg: "200px"}}>
+        <Flex align="center" justify ='space-between' width={{base: "autho", lg: "200px"}}>
                     <Flex align ="center">
                         {/*<Icon as={FaRedhat} /> */}
                         <Text fontSize= '12'> Field of Study</Text>
